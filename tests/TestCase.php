27b780954c32f949415d8edebfec2e76f8ebfb50
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-02-21 21:53:11 +1100

    Seed database on each test, allow multiple authed users, remove auth helper

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,11 +8,29 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    public function asAuthorizedUser()
+    /**
+     * Indicates whether the default seeder should run before each test.
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    public function asAuthorizedUser(string $id = 'a')
     {
-        $headers = ['Authorization' => 'Bearer ' . env('TEST_PERSONAL_ACCESS_TOKEN', 'create a PAT and add it here')];
+        $tokens = [
+            'a' => env('TEST_PERSONAL_ACCESS_TOKEN_A', ''),
+            'b' => env('TEST_PERSONAL_ACCESS_TOKEN_B', '')
+        ];
+
+        $headers = ['Authorization' => 'Bearer ' . $tokens[$id]];
         $this->defaultHeaders = array_merge($this->defaultHeaders, $headers);
 
+        return $this;
+    }
+
+    public function withoutAuthorization()
+    {
+        unset($this->defaultHeaders['Authorization']);
         return $this;
     }
 }
