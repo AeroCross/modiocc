125ac27a2fe73dcd2a6203e745aa961ce00dc647
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-02-21 15:11:22 +1100

    Convenience method for authorised requests for tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function asAuthorizedUser()
+    {
+        $headers = ['Authorization' => 'Bearer ' . env('TEST_PERSONAL_ACCESS_TOKEN', 'create a PAT and add it here')];
+        $this->defaultHeaders = array_merge($this->defaultHeaders, $headers);
+
+        return $this;
+    }
 }
