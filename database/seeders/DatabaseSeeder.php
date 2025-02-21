8b611f0bc3f570c0bcae47c0c08feb03500b207d
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-02-21 21:52:27 +1100

    Update seeder to have a comparison user
    This new user will be what tests if users can modify other user's stuff.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,18 +14,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
+        // These are meant to be users that allow the test suite to run.
         $firstUser = User::factory()->has(Game::factory()->count(3))
             ->create([
-                'name' => 'Test User',
-                'email' => 'test@example.com',
+                'name' => 'Test User A',
+                'email' => 'test_a@example.com',
                 'password' => 'asdf'
             ]);
 
         $firstUser->tokens()->create([
             'name' => 'modio',
             'token' => hash('sha256', 'asdf'),
+        ]);
+
+        $secondUser = User::factory()->has(Game::factory()->count(5))
+            ->create([
+                'name' => 'Test User B',
+                'email' => 'test_b@example.com',
+                'password' => 'qwerty'
+            ]);
+
+        $secondUser->tokens()->create([
+            'name' => 'mytoken',
+            'token' => hash('sha256', 'qwerty'),
         ]);
     }
 }
