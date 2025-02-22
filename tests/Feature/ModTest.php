a414abfa3e3aa2a03e4bf51ba2f3ef40252d1b39
Mario Cuba <942337+AeroCross@users.noreply.github.com>
2025-02-22 13:07:07 +1100

    Format + fix read tests

--- tests/Feature/ModTest.php
+++ tests/Feature/ModTest.php
@@ -2,15 +2,16 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Game;
-use App\Models\Mod;
 use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class ModTest extends TestCase
 {
-    public function testBrowseSucceeds() : void
+    use RefreshDatabase;
+
+    public function testBrowseSucceeds(): void
     {
         // todo update this test to assert that a paginated response was given.
         //  in order for this test to pass, you will need to seed at least 1 game
@@ -19,7 +20,7 @@ class ModTest extends TestCase
         $mod = Game::query()->where('game', '=', $game->id)->first();
 
         $this
-            ->get('games/'.$game->id.'/mods')
+            ->get('games/' . $game->id . '/mods')
             ->assertStatus(Response::HTTP_OK)
             ->assertJsonStructure([
                 'id',
@@ -36,14 +37,14 @@ class ModTest extends TestCase
             ]);
     }
 
-    public function testCreateSucceedsWhileAuthenticated() : void
+    public function testCreateSucceedsWhileAuthenticated(): void
     {
         $game = Game::inRandomOrder()->first();
 
         // todo this endpoint must be secured by user authentication, modify the post call
         //   below to include the required header or URL parameter to achieve that
         $this
-            ->post('games/'.$game->id.'/mods', [
+            ->post('games/' . $game->id . '/mods', [
                 'name' => 'Lightsaber'
             ])
             ->assertStatus(Response::HTTP_CREATED)
@@ -62,51 +63,42 @@ class ModTest extends TestCase
             ]);
     }
 
-    public function testReadSucceeds() : void
+    public function testReadSucceeds(): void
     {
-        // todo create the game that we are going to add a mod to, adding the required authentication
-        $response = $this->post('games', [
-            'name' => 'Rogue Knight'
-        ])->assertStatus(Response::HTTP_CREATED);
-
-        // todo create the mod against the game we created above, add your auth
-        $response = $this->post('games/'.$response->json('id').'/mods', [
-            'name' => 'Lightsaber'
-        ])->assertStatus(Response::HTTP_CREATED);
-
-        // view the mod
         $this
-            ->get('games/'.$response->json('id').'/mods/'.$response->json('id'))
+            ->getJson('/api/games/1/mods/1')
             ->assertStatus(Response::HTTP_OK)
             ->assertJsonStructure([
-                'id',
-                'name',
-                'game_id',
-                'user_id',
-                'created_at',
-                'updated_at'
-            ])
-            ->assertJsonFragment([
-                'name' => 'Lightsaber',
-                // todo assert game is valid
-                // todo assert user is valid
+                'data' => [
+                    'id',
+                    'name',
+                    'game_id',
+                    'user_id',
+                    'created_at',
+                    'updated_at'
+                ]
             ]);
-
-
     }
 
-    public function testCreateFailsWhileUnauthenticated() : void
+    public function testReadFailsWhenModDoesNotBelongToGame(): void
+    {
+        $this
+            ->getJson('/api/games/1/mods/13') // Mod ID 12 belongs to Game ID 2
+            ->assertStatus(Response::HTTP_NOT_FOUND);
+    }
+
+    public function testCreateFailsWhileUnauthenticated(): void
     {
         $game = Game::inRandomOrder()->first();
 
         $this
-            ->post('games/'.$game->id.'/mods', [
+            ->postJson('/api/games/' . $game->id . '/mods', [
                 'name' => 'Lightsaber'
             ])
             ->assertStatus(Response::HTTP_UNAUTHORIZED);
     }
 
-    public function testUpdateSucceedsWhileAuthenticated() : void
+    public function testUpdateSucceedsWhileAuthenticated(): void
     {
         // todo again create the game, include the auth.
         $response = $this->post('games', [
@@ -114,13 +106,13 @@ class ModTest extends TestCase
         ])->assertStatus(Response::HTTP_CREATED);
 
         // todo create the mod, include the auth.
-        $response = $this->post('games/'.$response->json('id').'/mods', [
+        $response = $this->post('games/' . $response->json('id') . '/mods', [
             'name' => 'Lightsaber'
         ])->assertStatus(Response::HTTP_CREATED);
 
         // todo update the game, include the auth.
         $this
-            ->put('games/'.$mod->id.'/mods/'.$game->id, [
+            ->put('games/' . $mod->id . '/mods/' . $game->id, [
                 'name' => 'Lightsabers (Full set)'
             ])
             ->assertStatus(Response::HTTP_OK)
@@ -139,7 +131,7 @@ class ModTest extends TestCase
             ]);
     }
 
-    public function testUpdateFailsWhileUnauthenticated() : void
+    public function testUpdateFailsWhileUnauthenticated(): void
     {
         // todo again create the game, include VALID auth here, just to create the game successfully.
         $response = $this->post('games', [
@@ -148,13 +140,13 @@ class ModTest extends TestCase
 
         // this however should fail with 401 Unauthorized, as expected
         $this
-            ->put('games/'.$response->json('id').'/mods/'.$mod->id, [
+            ->put('games/' . $response->json('id') . '/mods/' . $mod->id, [
                 'name' => 'Lightsabers (Full set)'
             ])
             ->assertStatus(Response::HTTP_UNAUTHORIZED);
     }
 
-    public function testDeleteSucceedsWhileAuthenticated() : void
+    public function testDeleteSucceedsWhileAuthenticated(): void
     {
         // todo again create the game, include the auth just so that we have something to attempt to delete.
         $response = $this->post('games', [
@@ -164,22 +156,22 @@ class ModTest extends TestCase
         $gameId = $response->json('id');
 
         // todo create the mod, include the auth.
-        $response = $this->post('games/'.$gameId.'/mods', [
+        $response = $this->post('games/' . $gameId . '/mods', [
             'name' => 'Lightsaber'
         ])->assertStatus(Response::HTTP_CREATED);
 
         // and just for sanity we make sure it actually got created
         $this
-            ->get('games/'.$gameId.'/mods/'.$response->json('id'))
+            ->get('games/' . $gameId . '/mods/' . $response->json('id'))
             ->assertStatus(Response::HTTP_OK);
 
         // then we finally attempt to delete it without authentication present
         $this
-            ->delete('games/'.$gameId.'/mods/'.$response->json('id'))
+            ->delete('games/' . $gameId . '/mods/' . $response->json('id'))
             ->assertStatus(Response::HTTP_NO_CONTENT);
     }
 
-    public function testDeleteFailsWhileUnauthenticated() : void
+    public function testDeleteFailsWhileUnauthenticated(): void
     {
         // todo again create the game, include the auth just so that we have something to attempt to delete.
         $response = $this->post('games', [
@@ -189,18 +181,18 @@ class ModTest extends TestCase
         $gameId = $response->json('id');
 
         // todo create the mod, include the auth.
-        $response = $this->post('games/'.$gameId.'/mods', [
+        $response = $this->post('games/' . $gameId . '/mods', [
             'name' => 'Lightsaber'
         ])->assertStatus(Response::HTTP_CREATED);
 
         // and just for sanity we make sure it actually got created
         $this
-            ->get('games/'.$gameId.'/mods/'.$response->json('id'))
+            ->get('games/' . $gameId . '/mods/' . $response->json('id'))
             ->assertStatus(Response::HTTP_OK);
 
         // then we finally attempt to delete it without authentication present
         $this
-            ->delete('games/'.$gameId.'/mods/'.$response->json('id'))
+            ->delete('games/' . $gameId . '/mods/' . $response->json('id'))
             ->assertStatus(Response::HTTP_UNAUTHORIZED);
     }
 }
